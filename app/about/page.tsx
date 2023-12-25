9b4267e7f36a4b47ecb730f1485bcd08cf9386c7
Luiz <luiz_higuti@hotmai.com>
2023-12-25 16:45:46 -0300

    test using react useRef

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,19 +6,36 @@ import Projects from '@/components/about/Projects';
 import { AppContext } from '@/contexts/app.context';
 import Flex from '@/lib/components/Flex';
 import styles from '@/styles/about.module.scss';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 
 export default function _About() {
-  const context = useContext(AppContext);
+  const { currentSection } = useContext(AppContext);
+  const aboutRef = useRef<HTMLElement>(null);
+  const principlesRef = useRef<HTMLElement>(null);
+  const projectsRef = useRef<HTMLElement>(null);
+  const contactsRef = useRef<HTMLElement>(null);
+
+  function scrollIntoRef() {}
 
   useEffect(() => {
     // get the element by id and use scrollIntoView
-    if (!context.currentSection) return;
-    const elem = document.getElementById(context.currentSection);
-    console.log('useEffect', context.currentSection);
-    console.log('useEffect', elem);
-    elem?.scrollIntoView({ behavior: 'smooth' });
-  }, [context]);
+    if (!currentSection) return;
+
+    switch (currentSection) {
+      case 'about':
+        aboutRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'principles':
+        principlesRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'projects':
+        projectsRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'contacts':
+        contactsRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+    }
+  }, [currentSection]);
 
   return (
     <main className={styles.about}>
@@ -28,16 +45,16 @@ export default function _About() {
         bg='surface'
         px={{ base: 0, md: 12 }}
       >
-        <section id='about'>
+        <section id='about' ref={aboutRef}>
           <AboutUs />
         </section>
-        <section id='principles'>
+        <section id='principles' ref={principlesRef}>
           <Principles />
         </section>
-        <section id='projects'>
+        <section id='projects' ref={projectsRef}>
           <Projects />
         </section>
-        <section id='contacts'>
+        <section id='contacts' ref={contactsRef}>
           <Contacts />
         </section>
       </Flex>
